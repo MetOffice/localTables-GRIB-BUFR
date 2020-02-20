2002347cca7ac577073acc3d7f24c45ad06885e8
marqh <markh@metarelate.net>
2020-02-20 17:05:19 +0000

    concept scheme definition

--- grib/grib2/makeG2Entities.py
+++ grib/grib2/makeG2Entities.py
@@ -1,11 +1,19 @@
 import csv
 import os
 
+conceptSchemeTp = ('@prefix skos:  <http://www.w3.org/2004/02/skos/core#> . \n'
+                   '@prefix dct: <http://purl.org/dc/terms/> . \n'
+                   '@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n\n'
+                   '<mo--74> a skos:ConceptScheme \n'
+                   '\trdfs:label "Met Office Local Table definitions"@en ;\n'
+                   '\tdct:description "Met Office Local Table definitions '
+                   'Extending WMO GRIB2 code table 4.2 for cases where the '
+                   'defined centre number is 74."@en .\n')
 
 conceptTemplate = ('@prefix skos:  <http://www.w3.org/2004/02/skos/core#> . \n'
                    '@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n\n'
-                   '<{d}-{c}-{n}> a skos:Concept \n'
-                   '\trdfs:label "{label}"@en \n'
+                   '<{d}-{c}-{n}> a skos:Concept ;\n'
+                   '\trdfs:label "{label}"@en ;\n'
                    '\t<http://metarelate.net/vocabulary/index.html#identifier> '
                    '\t<http://codes.wmo.int/def/common/edition> , '
                    '\t<http://codes.wmo.int/def/common/centre> , '
@@ -18,9 +26,14 @@ conceptTemplate = ('@prefix skos:  <http://www.w3.org/2004/02/skos/core#> . \n'
                    '\t<http://codes.wmo.int/def/grib2/category> <http://codes.wmo.int/grib2/codeflag/4.1/{d}-{c}> ;\n'
                    '\t<http://codes.wmo.int/def/grib2/discipline> <http://codes.wmo.int/grib2/codeflag/0.0/{d}> ;\n'
                    '\t<http://codes.wmo.int/def/grib2/parameter> {n} ;\n'
-                   '\tskos:related <http://reference.metoffice.gov.uk/um/stash/m01s{ss}i{si}>')
+                   '\tskos:related <http://reference.metoffice.gov.uk/um/stash/m01s{ss}i{si}> ;\n'
+                   '\t.\n')
 
 root_path = os.path.dirname(__file__)
+
+with open(os.path.join(root_path, 'mo--74.ttl')) as csf:
+    csf.write(conceptSchemeTp)
+
 with open(os.path.join(root_path, 'GRIB2LocalTable.csv')) as cf:
     greader = csv.DictReader(cf)
     ttlpath = os.path.join(root_path, 'mo--74')
