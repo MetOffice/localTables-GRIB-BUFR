f999907b8ebffab394880ee24e7d97f03233d649
marqh <mark.hedley@metoffice.gov.uk>
2021-04-06 12:51:09 +0000

    depth max current + dct prefix (#35)

--- grib/grib2/makeG2Entities.py
+++ grib/grib2/makeG2Entities.py
@@ -25,26 +25,28 @@ conceptScheme45 = ('@prefix skos:  <http://www.w3.org/2004/02/skos/core#> . \n'
                    'Extending WMO GRIB2 code table 4.5 for cases where the '
                    'defined centre number is 74."@en .\n')
 
-conceptTemplate42 = ('@prefix skos:  <http://www.w3.org/2004/02/skos/core#> . \n'
-                   '@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n\n'
-                   '<{d}-{c}-{n}> a skos:Concept ;\n'
-                   '\trdfs:label "{label}"@en ;\n'
-                   '\t<http://metarelate.net/vocabulary/index.html#identifier> '
-                   '\t<http://codes.wmo.int/def/common/edition> , '
-                   '\t<http://codes.wmo.int/def/common/centre> , '
-                   '\t<http://codes.wmo.int/def/grib2/discipline> , '
-                   '\t<http://codes.wmo.int/def/grib2/parameter> , '
-                   '\t<http://codes.wmo.int/def/grib2/category> ;\n'
-                   '\t<http://codes.wmo.int/def/common/centre> <http://codes.wmo.int/common/centre/74> ;\n'
-                   '\t<http://codes.wmo.int/def/common/edition> <http://codes.wmo.int/codeform/grib2> ;\n'
-                   '{u}'
-                   '\t<http://codes.wmo.int/def/grib2/category> <http://codes.wmo.int/grib2/codeflag/4.1/{d}-{c}> ;\n'
-                   '\t<http://codes.wmo.int/def/grib2/discipline> <http://codes.wmo.int/grib2/codeflag/0.0/{d}> ;\n'
-                   '\t<http://codes.wmo.int/def/grib2/parameter> {n} ;\n'
-                   '\tskos:related <http://reference.metoffice.gov.uk/um/stash/m01s{ss:02d}i{si:03d}> ;\n'
-                   '\t.\n')
+conceptTemplate42 = ('@prefix dct: <http://purl.org/dc/terms/> . \n'
+                     '@prefix skos:  <http://www.w3.org/2004/02/skos/core#> . \n'
+                     '@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n\n'
+                     '<{d}-{c}-{n}> a skos:Concept ;\n'
+                     '\trdfs:label "{label}"@en ;\n'
+                     '\t<http://metarelate.net/vocabulary/index.html#identifier> '
+                     '\t<http://codes.wmo.int/def/common/edition> , '
+                     '\t<http://codes.wmo.int/def/common/centre> , '
+                     '\t<http://codes.wmo.int/def/grib2/discipline> , '
+                     '\t<http://codes.wmo.int/def/grib2/parameter> , '
+                     '\t<http://codes.wmo.int/def/grib2/category> ;\n'
+                     '\t<http://codes.wmo.int/def/common/centre> <http://codes.wmo.int/common/centre/74> ;\n'
+                     '\t<http://codes.wmo.int/def/common/edition> <http://codes.wmo.int/codeform/grib2> ;\n'
+                     '{u}'
+                     '\t<http://codes.wmo.int/def/grib2/category> <http://codes.wmo.int/grib2/codeflag/4.1/{d}-{c}> ;\n'
+                     '\t<http://codes.wmo.int/def/grib2/discipline> <http://codes.wmo.int/grib2/codeflag/0.0/{d}> ;\n'
+                     '\t<http://codes.wmo.int/def/grib2/parameter> {n} ;\n'
+                     '{stc}{desc}'
+                     '\t.\n')
 
-conceptTemplate45 = ('@prefix skos:  <http://www.w3.org/2004/02/skos/core#> . \n'
+conceptTemplate45 = ('@prefix dct: <http://purl.org/dc/terms/> . \n'
+                     '@prefix skos:  <http://www.w3.org/2004/02/skos/core#> . \n'
                      '@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n\n'
                      '<{cf}> a skos:Concept ;\n'
                      '\trdfs:label "{label}"@en ;\n'
@@ -74,13 +76,20 @@ def main():
             if entity['Unit']:
                 ustr = '\t<http://codes.wmo.int/def/common/unit> "{}" ;\n'
                 ustr = ustr.format(entity['Unit'])
+            stcstr = ''
+            if entity['STASH code']:
+                tstr = ('\tskos:related <http://reference.metoffice.gov.uk/'
+                        'um/stash/m01s{ss:02d}i{si:03d}> ;\n')
+                stcstr = tstr.format(ss=int(int(entity['STASH code'])/1000),
+                                     si=int(entity['STASH code'])%1000)
+            descstr = ''
+            if entity['Description']:
+                descstr = '\tdct:description "{}"@en ;\n'.format(entity['Description'])
             dstr = conceptTemplate42.format(d=entity['Discipline'],
                                                 c=entity['Category'],
                                                 n=entity['Number'],
                                                 label=entity['Parameter'],
-                                                u=ustr,
-                                                ss=int(int(entity['STASH code'])/1000),
-                                                si=int(entity['STASH code'])%1000)
+                                                u=ustr, stc=stcstr, desc=descstr)
             if os.path.exists(fpath):
                 with open(fpath, 'r', encoding='utf-8') as fh:
                     fhdef = fh.read()
