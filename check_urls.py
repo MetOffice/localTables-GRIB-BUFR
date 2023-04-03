6a9d25e0f458c648fd52fdb9f61cbc0302640a7a
marqh <mark.hedley@metoffice.gov.uk>
2023-04-03 09:20:51 +0000

    work flow updates

--- check_urls.py
+++ check_urls.py
@@ -24,6 +24,18 @@ remote that are not in the source tree.
 
 """
 
+def authenticate(session, base, userid, pss):
+    auth = session.post('{}/system/security/apilogin'.format(base),
+                        data={'userid':userid,
+                                'password':pss})
+    if not auth.status_code == 200:
+        raise ValueError('auth failed')
+
+    return session
+
+with open('prodRegister', 'r', encoding='utf-8') as fh:
+    rooturl = fh.read().split('\n')[0].replace('http://', 'https://')
+
 uploads = {'PUT': [],
            'POST': []}
 
@@ -35,6 +47,14 @@ if outfile is not None:
         raise ValueError('outfile directory is not writeable: {}'.format(outfile))
     elif os.path.exists(outfile) and not os.access(outfile, os.W_OK):
         raise ValueError('outfile is not writeable: {}'.format(outfile))
+
+uname = os.environ.get('uname', None)
+passcode = os.environ.get('passcode', None)
+
+session = requests.Session()
+if uname is not None and passcode is not None:
+    session = authenticate(session, rooturl, uname, passcode)
+
 
 nofails = os.environ.get('nofails', None)
 if nofails is None or not nofails:
@@ -89,7 +109,7 @@ for f in glob.glob('**/*.ttl', recursive=True):
         identityURI = copy.copy(identity)
         def entity_exists(self):
             headers={'Accept':'text/turtle'}
-            regr = requests.get(identityURI, headers=headers)
+            regr = session.get(identityURI, headers=headers)
             try:
                 assert(regr.status_code == 200)
             except AssertionError:
@@ -109,26 +129,41 @@ for f in glob.glob('**/*.ttl', recursive=True):
         identityURI = copy.copy(identity)
         def entity_consistent(self):
             headers={'Accept':'text/turtle'}
-            regr = requests.get(identityURI, headers=headers)
+            regr = session.get(identityURI, headers=headers)
             ufile = '{}.ttl'.format(identityURI.split(rooturl)[1].lstrip('/'))
             if not nofails:
-                assert(regr.status_code == 200)
+                msg = '{} returned {} not 200'.format(identityURI,
+                                                      regr.status_code)
+                assert(regr.status_code == 200), msg
             if regr.status_code == 200:
-                expected = requests.get(identityURI, headers=headers)
-                expected_rdfgraph = rdflib.Graph()
-                expected_rdfgraph.parse(data=expected.text, format='n3')
-                # print(expected)
                 result_rdfgraph = rdflib.Graph()
                 # print(identityURI)
                 result_rdfgraph.parse(ufile, publicID=identityURI, format='n3')
+                expected = session.get(identityURI, headers=headers)
+                expected_rdfgraph = rdflib.Graph()
+                expected_rdfgraph.parse(data=expected.text, format='n3')
+                if os.path.exists(identityURI.split(rooturl)[1].lstrip('/')):
+                    # add in member relations from tree
+                    col_id, = result_rdfgraph.subjects(rdflib.RDF.type, rdflib.namespace.SKOS.Collection)
+                    for fname in glob.glob('{}/*.ttl'.format(identityURI.split(rooturl)[1].lstrip('/'))):
+                        member_id = rdflib.term.URIRef(u'{}/{}'.format(identityURI, fname.split('/')[-1].split('.ttl')[0]))
+                        result_rdfgraph.add((col_id, rdflib.namespace.SKOS.member, member_id))
+                        expected_rdfgraph.remove((member_id, None, None))
+                        expected_rdfgraph.remove((None, rdflib.namespace.RDFS.member, None))
+                        #expected_rdfgraph.remove((None, rdflib.term.URIRef('http://purl.org/linked-data/registry#subregister'), None))
+                # print(expected)
+                # do not check version info or date modified (owned by registry)
+                expected_rdfgraph.remove((None, rdflib.namespace.DCTERMS.modified, None))
+                expected_rdfgraph.remove((None, rdflib.namespace.OWL.versionInfo, None))
+                # print(expected)
                 self.check_result(result_rdfgraph, expected_rdfgraph, uploads,
                                   identityURI)
         return entity_consistent
 
     # skip uncheckable content, e.g. container registers
     # print(f)
-    if f in ['grib/grib2/mo--74/4.2.ttl', 'grib/grib2/mo--74/4.5.ttl']:
-        continue
+    # if f in ['grib/grib2/mo--74/4.2.ttl', 'grib/grib2/mo--74/4.5.ttl']:
+    #     continue
 
     tname = 'test_consistent_{}'.format(relf.replace('/', '_'))
     setattr(TestContentsConsistency, tname, make_another_test(f))
